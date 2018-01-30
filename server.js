4fb43d4ebe8509e9233dd39fc4d18732c008f796
kyhrikard <rikard.carlsson@student.kyh.se>
2018-01-30 11:56:49 +0100

    Fix typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
-const bodyParser = reuquire('body-parser')
+const bodyParser = require('body-parser')
 const { Client } = require('pg')
 const client = new Client({
     connectionString: process.env.DATABASE_URL || 'postgres://rshfeczvrlrapg:ad2d64520322decff1e2fd7b46ba323fe0b71add82638bc3bc5d9bbebc0b2f63@ec2-54-204-43-7.compute-1.amazonaws.com:5432/dc5lesh6qum9mc',

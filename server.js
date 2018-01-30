fd8d57c94f875aa68ae0d7fafaa2a0e363e51586
kyhrikard <rikard.carlsson@student.kyh.se>
2018-01-30 11:55:05 +0100

    Second commit

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const bodyParser = reuquire('body-parser')
+const { Client } = require('pg')
+const client = new Client({
+    connectionString: process.env.DATABASE_URL || 'postgres://rshfeczvrlrapg:ad2d64520322decff1e2fd7b46ba323fe0b71add82638bc3bc5d9bbebc0b2f63@ec2-54-204-43-7.compute-1.amazonaws.com:5432/dc5lesh6qum9mc',
+    ssl: true
+})
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
@@ -12,4 +18,15 @@ app.get('/', (request, response) => {
     response.write('<p>This is the dev Nestr backend</p>')
     response.write('<p>Here we are building our web API</p>')
     response.end()
+})
+
+client.connect();
+
+app.get('/nests', (request, response) => {
+    client.query('SELECT * FROM nests', (err, res) => {
+        if (err)
+            console.log(err)
+        else
+            response.json(res.rows)
+    })
 })

14c734aafac336e211f5a9ccfccdfde2043be907
kyhrikard <rikard.carlsson@student.kyh.se>
2018-02-02 15:48:35 +0100

    Add post fuctionality

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 5000
 const bodyParser = require('body-parser')
 const { Client } = require('pg')
+const app = express()
+const port = process.env.PORT || 5000
 const client = new Client({
     connectionString: process.env.DATABASE_URL || 'postgres://rshfeczvrlrapg:ad2d64520322decff1e2fd7b46ba323fe0b71add82638bc3bc5d9bbebc0b2f63@ec2-54-204-43-7.compute-1.amazonaws.com:5432/dc5lesh6qum9mc',
     ssl: true
 })
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(cors())
 
 app.listen(port, () => {
@@ -78,6 +79,7 @@ app.put('/api/nests/:id', (request, response) => {
     const text = 'UPDATE nest SET name = COALESCE($2, name), latitude = COALESCE($3, latitude), longitude = COALESCE($4, longitude) WHERE id=$1'
     const values = [request.params.id, request.body.name, request.body.latitude, request.body.longitude]
 
+    // Check if request is empty
     let emptyRequest = true
 
     for (let index = 1; index < values.length; index++) {
@@ -125,14 +127,29 @@ app.post('/api/players', (request, response) => {
     const text = 'INSERT INTO player(username, password, teamid) VALUES($1, $2, $3)'
     const values = [request.body.username, request.body.password, request.body.teamid]
 
-    client.query(text, values, (err, res) => {
-        if (err) {
-            response.status(400)
-            response.json(err.detail)
-        } else {
-            response.status(201)
-            response.json('Player created')
-        }
-    })
+    //Check if request contains empty fields
+    let emptyRequestFields = false
+
+    for (let index = 0; index < values.length; index++) {
+        console.log(values[index])
+        if (typeof values[index] === 'undefined')
+            emptyRequestFields = true
+    }
+
+    if (emptyRequestFields) {
+        response.status(400)
+        response.json('Your request contains empty fields')
+    }
+    else {
+        client.query(text, values, (err, res) => {
+            if (err) {
+                response.status(400)
+                response.json(err.detail)
+            } else {
+                response.status(201)
+                response.json('Player created')
+            }
+        })
+    }
 
 })

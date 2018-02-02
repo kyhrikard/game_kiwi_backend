ded114e12cf0efe86542e3b9b97a8ce60b85a96c
kyhrikard <rikard.carlsson@student.kyh.se>
2018-02-02 13:25:47 +0100

    Add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 const bodyParser = require('body-parser')
@@ -9,6 +10,7 @@ const client = new Client({
 })
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors)
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
